f9958aef0e8e4976ffe587088138bf3d04591ba7
Tango <ats3216@uw.edu>
2020-01-23 19:39:42 -0800

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React better you person
         </a>
       </header>
     </div>
